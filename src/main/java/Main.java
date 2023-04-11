3ba98805dbf9699fcac074e8006c227518b56c0f
Igor Avdonin <thathorizon@gmail.com>
2023-04-11 10:48:00 +0300

    Init commit
    (cherry picked from commit 7b8f615cb96577f6293a5ba67192e8942d69e00b)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 public class Main {
 
 
     public static void main(String[] args) {
         System.out.println("На сколько человек поделить счет?\nВведите количество:");
+=======
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+>>>>>>> 7b8f615 (Init commit)
     }
 }
