524e899c7ed68dfbc6eb774748dc4c5dd49710aa
azimovin01 <azimovin01@yandex.ru>
2024-04-11 14:32:18 +0300

    delete all

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,5 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Scanner;
-
 public class Main {
-
-
     public static void main(String[] args) {
         System.out.println("На сколько человек поделить счет?\nВведите количество:");
-        Calculator calculator = new Calculator();
-        calculator.calc();
     }
 }
